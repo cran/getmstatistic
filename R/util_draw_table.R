a0989a51007c688769835b4647e7e7fd5a080583
Lerato E Magosi <lmagosi@well.ox.ac.uk>
2017-06-08 22:08:47 +0000

    version 0.1.1

--- R/util_draw_table.R
+++ R/util_draw_table.R
@@ -17,24 +17,27 @@
 #' @param heading A string. Table title.
 #' @param ... Further arguments to control the gtable.
 #'
-#'  @examples
-#'  # note: not exported hence examples are not run
-#'  library(gridExtra)
+#' @examples
+#' # note: not exported hence examples are not run
+#' library(gridExtra)
 #'
-#'  # Table of iris values
-#'  iris_dframe <- head(iris)
-#'  title_iris_dframe <- paste("Table: Length and width measurements (cm) of sepals ",
+#' \donttest{
+#'
+#' # Table of iris values
+#' iris_dframe <- head(iris)
+#' title_iris_dframe <- paste("Table: Length and width measurements (cm) of sepals ",
 #'                        "and petals, for 50 flowers from 3 species of iris ", 
 #'                        "(setosa, versicolor, and virginica). ", sep="")
-#'  table_influential_studies <-  draw_table(body = iris_dframe, heading = title_iris_dframe)
+#' table_influential_studies <-  draw_table(body = iris_dframe, heading = title_iris_dframe)
 #' 
-#'  # Table of mtcars values 
-#'  mtcars_dframe <- head(mtcars)
-#'  title_mtcars_dframe <- paste("Table: Motor Trend US magazine (1974) automobile ",
+#' # Table of mtcars values 
+#' mtcars_dframe <- head(mtcars)
+#' title_mtcars_dframe <- paste("Table: Motor Trend US magazine (1974) automobile ",
 #'                         "statistics for fuel consumption, automobile ", 
 #'                         "design and performance. ", sep="")
-#'  table_influential_studies <-  draw_table(body = mtcars_dframe, 
+#' table_influential_studies <-  draw_table(body = mtcars_dframe, 
 #'                                           heading = title_mtcars_dframe)
+#'}
 
 #' # @export
 draw_table <- function(body, heading, ...) {
@@ -76,9 +79,9 @@ draw_table <- function(body, heading, ...) {
       ...
     )
     
-    title <- grid::textGrob(heading, gp=grid::gpar(fontsize=12), just="centre")
+    title <- grid::textGrob(heading, gp=grid::gpar(fontsize=12), vjust=0)
     
-    padding <- grid::unit(0.4,"npc")
+    padding <- grid::unit(0.2,"npc")
     
     table <- gtable::gtable_add_rows(table, heights = grid::grobHeight(title) + padding, pos = 0)
     
